b98ed2e7a19e57601987cfba68971d602fcffa58
munyeoll <munyeol08@gmail.com>
2023-05-16 00:04:00 +0900

    First commit

---
+++ src/main/java/com/demo/pccm/PccmApplication.java
@@ -0,0 +1,13 @@
+package com.demo.pccm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PccmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PccmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/pccm/PccmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.pccm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PccmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
