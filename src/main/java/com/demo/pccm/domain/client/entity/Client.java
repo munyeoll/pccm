010276f94323b8c126d6e8a97feaa08c135943fd
munyeoll <munyeol08@gmail.com>
2023-05-16 01:20:54 +0900

    Feat : Client entity, repository 생성
    고객정보 관리를 위한 entity 및 repository 생성
    초기버전

---
+++ src/main/java/com/demo/pccm/domain/client/entity/Client.java
@@ -0,0 +1,65 @@
+package com.demo.pccm.domain.client.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@SequenceGenerator(
+        name = "CLIENT_SEQ_GENERATOR",
+        sequenceName = "CLIENT_SEQ",
+        initialValue = 1,
+        allocationSize = 1
+)
+@Getter
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "CLIENT_SEQ_GENERATOR"
+    )
+    private Long id;
+
+    @Column(nullable = false, length = 15)
+    private String clientNo;
+
+    @Column(nullable = false, length = 30)
+    private String clientName;
+
+    @Column(length = 20)
+    private String phoneNo;
+
+    @Column(length = 50)
+    private String emailAddr;
+
+    @Column(nullable = false, length = 8)
+    private String beginYmd;
+
+    @Column(nullable = false, length = 8)
+    private String endYmd;
+
+    @Column(length = 1)
+    private String deleteYn;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+    private LocalDateTime deletedDate;
+
+    @Builder
+    public Client(String clientNo, String clientName, String phoneNo, String emailAddr, String beginYmd, String endYmd) {
+        this.clientNo = clientNo;
+        this.clientName = clientName;
+        this.phoneNo = phoneNo;
+        this.emailAddr = emailAddr;
+        this.beginYmd = beginYmd;
+        this.endYmd = endYmd;
+        this.deleteYn = "N";
+        this.createdDate = LocalDateTime.now();
+    }
+
+}
