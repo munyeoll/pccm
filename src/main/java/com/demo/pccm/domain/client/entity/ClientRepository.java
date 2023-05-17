6828f96208441a699598b0fee66ea3341fbf1a2e
munyeoll <munyeol08@gmail.com>
2023-05-17 21:30:11 +0900

    feat : 고객번호로 조회하는 메소드 추가
    중복고객 확인용
    todo : begin/endYmd 를 이용하여 기간 중복까지 포함해서 중복검사 해야한다.

--- src/main/java/com/demo/pccm/domain/client/entity/ClientRepository.java
+++ src/main/java/com/demo/pccm/domain/client/entity/ClientRepository.java
@@ -2,5 +2,10 @@ package com.demo.pccm.domain.client.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    Optional<Client> findByClientNo(String clientNo);
+
 }
