728d150dc8a85861ff882eb56287fed62ffb877d
munyeoll <munyeol08@gmail.com>
2023-05-17 21:25:40 +0900

    feat : 커스텀 Exception 생성

---
+++ src/main/java/com/demo/pccm/global/exception/BusinessException.java
@@ -0,0 +1,13 @@
+package com.demo.pccm.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BusinessException extends RuntimeException {
+
+    private String status;
+    private String message;
+
+}
