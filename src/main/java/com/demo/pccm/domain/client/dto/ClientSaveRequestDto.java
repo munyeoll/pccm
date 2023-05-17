a0120d77586be532dc6ca5a8289e6dd4cfd08c9d
munyeoll <munyeol08@gmail.com>
2023-05-17 21:32:02 +0900

    feat : Create ClientSaveRequestDto
    신규 고객정보 등록 요청용 Dto

---
+++ src/main/java/com/demo/pccm/domain/client/dto/ClientSaveRequestDto.java
@@ -0,0 +1,32 @@
+package com.demo.pccm.domain.client.dto;
+
+import com.demo.pccm.domain.client.entity.Client;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor
+public class ClientSaveRequestDto {
+
+    private Long id;
+    private String clientNo;
+    private String clientName;
+    private String phoneNo;
+    private String emailAddr;
+    private String beginYmd;
+    private String endYmd;
+
+    public Client toEntity() {
+        return Client.builder()
+                .clientNo(clientNo)
+                .clientName(clientName)
+                .phoneNo(phoneNo)
+                .emailAddr(emailAddr)
+                .beginYmd(beginYmd)
+                .endYmd(endYmd)
+                .build();
+    }
+
+}
