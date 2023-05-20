a2a33512dee21c2b58f7c030b369fedcf2d04979
munyeoll <munyeol08@gmail.com>
2023-05-20 13:17:17 +0900

    feat : 기존 method mapping 정보 변경 및 신규 mapping 추가
    mapping url 에서 prefix 제거 ( ex: /save, /update ) 함.
      -> method 가 이미 동작을 표기 중이기 때문에
    
    추가 : delete, get(one, list)

--- src/main/java/com/demo/pccm/domain/client/api/ClientApiController.java
+++ src/main/java/com/demo/pccm/domain/client/api/ClientApiController.java
@@ -1,10 +1,13 @@
 package com.demo.pccm.domain.client.api;
 
+import com.demo.pccm.domain.client.dto.ClientDto;
 import com.demo.pccm.domain.client.dto.ClientSaveDto;
 import com.demo.pccm.domain.client.dto.ClientUpdateDto;
 import com.demo.pccm.domain.client.service.ClientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/client")
@@ -13,14 +16,29 @@ public class ClientApiController {
 
     private final ClientService clientService;
 
-    @PostMapping("/save")
+    @PostMapping
     public Long save(@RequestBody ClientSaveDto clientSaveDto) {
         return clientService.save(clientSaveDto);
     }
 
-    @PatchMapping("/update/{id}")
+    @PatchMapping("/{id}")
     public Long update(@PathVariable Long id, @RequestBody ClientUpdateDto clientUpdateDto) {
         return clientService.update(id, clientUpdateDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public Long delete(@PathVariable Long id) {
+        return clientService.delete(id);
+    }
+
+    @GetMapping("/{id}")
+    public ClientDto getClientById(@PathVariable Long id) {
+        return clientService.getClientById(id);
+    }
+
+    @GetMapping("/list")
+    public List<ClientDto> getClientList() {
+        return clientService.getClientList();
     }
 
 }
