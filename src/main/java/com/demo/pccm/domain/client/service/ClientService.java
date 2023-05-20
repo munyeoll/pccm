50eee09bde24d72ce082fc1da959b582faf24b90
munyeoll <munyeol08@gmail.com>
2023-05-20 13:20:10 +0900

    feat : 고객정보 삭제, 조회 기능 추가

--- src/main/java/com/demo/pccm/domain/client/service/ClientService.java
+++ src/main/java/com/demo/pccm/domain/client/service/ClientService.java
@@ -1,5 +1,6 @@
 package com.demo.pccm.domain.client.service;
 
+import com.demo.pccm.domain.client.dto.ClientDto;
 import com.demo.pccm.domain.client.dto.ClientUpdateDto;
 import com.demo.pccm.domain.client.dto.ClientSaveDto;
 import com.demo.pccm.domain.client.entity.Client;
@@ -9,6 +10,10 @@ import com.demo.pccm.global.exception.ErrorCode;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,6 +41,24 @@ public class ClientService {
                 clientUpdateDto.getEndYmd()
         );
         return client.getId();
+    }
+
+    public Long delete(Long id) {
+        Client client = clientRepository.findById(id).orElseThrow(() ->
+                new BusinessException(ErrorCode.INVALID_PARAMETER));
+        client.delete();
+        return id;
+    }
+
+    public ClientDto getClientById(Long id) {
+        Client client = clientRepository.findById(id).orElseThrow(() ->
+                new BusinessException(ErrorCode.INVALID_PARAMETER));
+        return new ClientDto(client);
+    }
+
+    public List<ClientDto> getClientList() {
+        Optional<Client> clientList = clientRepository.findByDeleteYn("N");
+        return clientList.stream().map(client -> new ClientDto(client)).collect(Collectors.toList());
     }
 
     public boolean checkDuplicateClientNo(String clientNo) {
