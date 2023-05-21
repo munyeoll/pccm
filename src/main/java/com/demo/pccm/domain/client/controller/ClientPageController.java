a95307c8ca546b8027775ac44096937c0f731e03
munyeoll <munyeol08@gmail.com>
2023-05-21 15:36:46 +0900

    feat : 화면 호출용 page controller 생성

---
+++ src/main/java/com/demo/pccm/domain/client/controller/ClientPageController.java
@@ -0,0 +1,16 @@
+package com.demo.pccm.domain.client.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/client")
+@Controller
+public class ClientPageController {
+
+    @GetMapping("/list")
+    public String clientList() {
+        return "client/client-list";
+    }
+
+}
